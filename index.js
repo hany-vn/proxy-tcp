d08b0db86044de8dabe4948497fc4d11b3a6646f
Hany <quenmattroi@gmail.com>
2024-07-29 15:20:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // Cấu hình
-const LOCAL_PORT = 14447; // Cổng của máy chủ proxy
+const LOCAL_PORT = 14447; //a Cổng của máy chủ proxy
 const TARGET_HOST = "103.67.197.251"; // Địa chỉ IP hoặc tên miền của máy chủ TCP đích
 const TARGET_PORT = 14447; // Cổng của máy chủ TCP đích
 
@@ -14,17 +14,21 @@ const server = net.createServer((clientSocket) => {
           console.log("Connected to target server");
      });
 
-     // Khi nhận dữ liệu từ client, chuyển tiếp đến máy chủ đích
-     clientSocket.on("data", (data) => {
-          console.log(`Forwarding data to target server: ${data}`);
-          targetSocket.write(data);
-     });
+     // // Khi nhận dữ liệu từ client, chuyển tiếp đến máy chủ đích
+     // clientSocket.on("data", (data) => {
+     //      console.log(`Forwarding data to target server: ${data}`);
+     //      targetSocket.write(data);
+     // });
 
-     // Khi nhận dữ liệu từ máy chủ đích, chuyển tiếp đến client
-     targetSocket.on("data", (data) => {
-          console.log(`Forwarding data to client: ${data}`);
-          clientSocket.write(data);
-     });
+     // // Khi nhận dữ liệu từ máy chủ đích, chuyển tiếp đến client
+     // targetSocket.on("data", (data) => {
+     //      console.log(`Forwarding data to client: ${data}`);
+     //      clientSocket.write(data);
+     // });
+
+     // Sử dụng pipe để chuyển tiếp dữ liệu giữa clientSocket và targetSocket
+     clientSocket.pipe(targetSocket);
+     targetSocket.pipe(clientSocket);
 
      // Xử lý khi kết nối bị đóng
      clientSocket.on("end", () => {
